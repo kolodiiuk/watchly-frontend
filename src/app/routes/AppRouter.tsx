54f5032c63e9819a06af4ec113a7f2de1dc46c8d
kolodiiuk <nkolodeyuk@gmail.com>
2026-05-06 17:56:01 +0300

    fix: app router

--- src/app/routes/AppRouter.tsx
+++ src/app/routes/AppRouter.tsx
@@ -42,6 +42,7 @@ const router = createBrowserRouter([
         path: 'episode/:episodeId',
         element: <EpisodePage />,
       },
+      {
         path: 'users/reset-password',
         element: <ResetPasswordPage />,
       },
